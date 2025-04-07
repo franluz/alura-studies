ff7755235c6cce50a8f53e95c1175d7ce7f94fd2
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-04-07 13:28:25 -0300

    correcao da parte de inclusao via state

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Button.module.scss'
-class Botao extends React.Component<{texto: string}> {
- 
+class Botao extends React.Component<{ texto: string, type?: "button" | "submit" | "reset" | undefined }> {
+
     render() {
+        const { type = "button" } = this.props;
         return (
-            <button className={style.botao} >
-                 {this.props.texto}
+            <button className={style.botao} type={type}>
+                {this.props.texto}
             </button>
         )
     }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import Cronometro from '../components/Crenometro';
 import style from './App.module.scss'
+import { ITarefa } from '../types/tarefa';
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>(
+    []
+  );
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
-      <Cronometro/>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
+      <Cronometro />
     </div>
   );
 }
